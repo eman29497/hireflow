b632cc600a7a33dacf89a9c5e48ad1b9d42c6043
eman <emansajjad863@gmail.com>
2026-07-13 17:25:51 +0500

    fix: update cors

--- backend/index.ts
+++ backend/index.ts
@@ -10,8 +10,9 @@ const app: Application = express();
 app.use(
   cors({
     origin: [
-      "https://hireflow-frontend-two.vercel.app",
+      "https://hireflow-frontend-two.vercel.app"
     ],
+    methods:['GET','POST','PUT','DELETE'],
     credentials: true,
   })
 );
